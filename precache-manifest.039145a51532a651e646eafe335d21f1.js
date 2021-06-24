b422901854013be5bfdfb2c3e0c79dd9287c5715
ansabiqbal <ansabiqbal903@gmail.com>
2021-06-24 15:49:43 +0500

    Updates

---
+++ precache-manifest.039145a51532a651e646eafe335d21f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9a1e139abf332ac4a18be8fa1adad1",
+    "url": "/react-travel-app/index.html"
+  },
+  {
+    "revision": "72b200abf3ce30813649",
+    "url": "/react-travel-app/static/css/main.64d57fa2.chunk.css"
+  },
+  {
+    "revision": "36128ff7f373e5beff7a",
+    "url": "/react-travel-app/static/js/2.1442e098.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-travel-app/static/js/2.1442e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b200abf3ce30813649",
+    "url": "/react-travel-app/static/js/main.09479ae7.chunk.js"
+  },
+  {
+    "revision": "b74c708de12741965029",
+    "url": "/react-travel-app/static/js/runtime-main.5f8619d1.js"
+  }
+]);
